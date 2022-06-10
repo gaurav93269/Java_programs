607d0a40d40708e38b86f5723b5aa0a579182bfd
gaurav93269 <103619206+gaurav93269@users.noreply.github.com>
2022-06-10 15:18:40 +0530

    Add files via upload

--- Single_inheritance.java
+++ Single_inheritance.java
@@ -1,37 +1,39 @@
-//single inheritance program...
-public class inheritance
+/*WAP to create a class which contain the method Palindrom .
+the second class will use the proerty of first class and display the output. */
+
+import java.util.*;
+public class Single_inheritance 
 {
-	public static void main(String[] args)
+   public static void pali()
+	{ 
+	   int r,rev=0,temp,num;
+       Scanner sc=new Scanner(System.in);
+   System.out.println("Enter the number to check palindrome");
+       num=sc.nextInt();
+       temp=num;    
+   while(num>0)
+{    
+   r=num%10;  //getting remainder  
+   rev=(rev*10)+r;    
+   num=num/10;    
+}    
+  if(temp==rev)    
+    System.out.println(" it is palindrome number ");    
+ else    
+    System.out.println("it is not palindrome");   
+
+     }
+}
+
+-----------------------------------------------------------------------------------------------
+public class Single_inheritance1 extends Single_inheritance
+{
+   public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		program_2 obj=new program_2();
-		obj.pro1();
-		obj.pro2();
-
-	}
-
+	   pali();
+		
+	}	
 }
-/*
-----------------------------------------------------------------------------
-public class program_1 
-{
-
-	public static void pro1()
-	{
-		// TODO Auto-generated method stub
-		System.out.println("hello gaurav yadav");
-   }
-}
---------------------------------------------------------------------
-
-public class program_2 extends program_1
-  {
-		public static void  pro2()
-			{
-				System.out.println("welcome to java world");
-			}
-  }
 
 
----------------------------------------------------------------------------------
-*/
